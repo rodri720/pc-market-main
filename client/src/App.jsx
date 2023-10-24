5fa321aabf718f313cc3f0029012974e3adc8c71
rodri720 <tomiyrami@gamil.com>
2023-10-24 13:44:12 -0300

    client listo

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  
+    
+    return (
+      <h1 className="bg-red-500 text-white font-bold;">
+        Hello world!
+      </h1>
+    )
+}
+
+export default App
